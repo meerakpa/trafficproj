c50155ff957f0b6c3395ce1aaffd34ab48749b1e
Meera <meerakpa@gmail.com>
2024-12-05 00:01:53 +0000

    updated app dev

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
 from flask import Flask, request, jsonify
 from google.cloud import bigquery
+import os
 
 app = Flask(__name__)
+os.environ["GOOGLE_CLOUD_PROJECT"] = "citric-gradient-439303-u3"
 client = bigquery.Client()
+
+@app.route('/')
+def home():
+    return "Traffic Prediction API is running!", 200
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     query = f"""
-        SELECT predicted_traffic_count
+        SELECT *
         FROM ML.PREDICT(
             MODEL `meerapdataset.traffic_predict_model`,
-            (SELECT '{data['date']}' AS traffic_count_date, '{data['location']}' AS location)
+            (SELECT 
+                1 AS ID, 
+                DATE('{data['date']}') AS traffic_count_date, 
+                '{data['location']}' AS location)
         )
     """
-    result = client.query(query).result()
-    predictions = [dict(row) for row in result]
-    return jsonify(predictions)
-
+    try:
+        result = client.query(query).result()
+        predictions = [dict(row) for row in result]
+        return jsonify(predictions)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
